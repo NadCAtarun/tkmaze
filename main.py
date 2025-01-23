f438f8074cec3bb0ab5a8aabf7a7b3ea7e16b9af
Nadia Cerezo <nadc.atarun@gmail.com>
2025-01-23 20:30:32 +0100

    Refactor Window class and add drawing functionality.
    Removed unnecessary parentheses in the Window class definition for clarity. Introduced Point and Line classes to enable geometry representation and added methods to draw lines on the canvas. Updated the main block to demonstrate drawing a rectangle using the new features.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 
-class Window():
+class Window:
     def __init__(self, width=1000, height=1000):
         self.window = Tk()
         self.window.title("Maze")
@@ -21,6 +21,34 @@ class Window():
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+
 if __name__ == "__main__":
     window = Window(800, 600)
+    p1 = Point(100, 100)
+    p2 = Point(100, 300)
+    p3 = Point(300, 300)
+    p4 = Point(300, 100)
+    l1 = Line(p1, p2)
+    l2 = Line(p2, p3)
+    l3 = Line(p3, p4)
+    l4 = Line(p4, p1)
+    window.draw_line(l1)
+    window.draw_line(l2)
+    window.draw_line(l3)
+    window.draw_line(l4)
     window.wait_for_close()
